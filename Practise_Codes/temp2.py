c136331fa86dfb757d4ae2ba00f5e3e1165f1409
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-07-03 15:02:57 +0530

    d

--- Practise_Codes/temp2.py
+++ Practise_Codes/temp2.py
@@ -1,36 +1,25 @@
-a = 0
-b = 1
-print("    ",a,"\n   ",b)
-rows = 3 
-  
-k = 2 * rows - 2  
+class Citizen:
 
-for i in range(0, rows):  
-     
-    for j in range(0, k):  
-        print(end=" ")  
-     
-    k = k - 1  
-    
-    for j in range(0, i + 1):  
-        for i in range(2):
-            c = a + b
-                         
-            print(c,end=' ')
-            a = b
-            b = c
+    def __init__(self,state,men,women, children ):
+        self.state = state
+        self.men = men  
+        self.women = women      
+        self.children = children
         
-    print("")  
-   
-k = rows - 2  
 
-for i in range(rows, -1, -1):  
+    def show(self):
+        print("State  =>",self.state)
+        print(" Men  =>",self.men)
+        print("Women =>",self.women)
+        print("Children =>",self.children
+
+s1 = Citizen('Bihar',23500,17237,5927)
+s2 = Citizen('Orissa',23658,24555,2364)
+s3 = Citizen('U P',36517,22617,6314)
+s4 = Citizen('Jharkhand',23254,19845).1336
     
-    for j in range(k, 0, -1):  
-        print(end=" ")  
-    
-    k = k + 1  
-     
-    for j in range(0, i + 1):  
-        print("* ", end="")  
-    print("")  
+
+print(s1)
+print(s2)
+print(Citizen)
+
