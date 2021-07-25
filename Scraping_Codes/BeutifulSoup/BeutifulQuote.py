f39879344357b97eb47057b79972fa30fd62dbaa
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-07-25 13:50:23 +0530

    Amazon/Flipkart Price Scrap

--- Scraping_Codes/BeutifulSoup/BeutifulQuote.py
+++ Scraping_Codes/BeutifulSoup/BeutifulQuote.py
@@ -16,13 +16,14 @@ table = soup.find('div', attrs = {'id':'all_quotes'})
 for row in table.findAll('div',
                          attrs = {'class':'col-6 col-lg-3 text-center margin-30px-bottom sm-margin-30px-top'}):
     quote = {}
+    print(row)
     quote['theme'] = row.h5.text
     quote['url'] = row.a['href']
     quote['img'] = row.img['src']
     quote['lines'] = row.img['alt'].split(" #")[0]
     quote['author'] = row.img['alt'].split(" #")[1]
     quotes.append(quote)
-   
+  
 filename = 'inspirational_quotes.csv'
 with open(filename, 'w', newline='') as f:
     w = csv.DictWriter(f,['theme','url','img','lines','author'])
