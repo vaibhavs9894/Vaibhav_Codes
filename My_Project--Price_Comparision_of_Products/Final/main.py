a47744bf1630412b25fbc0f58f2030e10d173679
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-07-28 20:12:25 +0530

    project

--- My_Project--Price_Comparision_of_Products/Final/main.py
+++ My_Project--Price_Comparision_of_Products/Final/main.py
@@ -5,33 +5,13 @@ import streamlit as st
 
 st.set_page_config(page_title='Price Comparision of Product')
 
-url=requests.get("https://www.flipkart.com/search?q=earbuds")
-html = url.content
-soup= BeautifulSoup(html,"html.parser")
-
-main=soup.find('div', class_='_1YokD2 _2GoDe3')
-
-data2=[]
-for vs in main.find_all('div', class_='_4ddWXP'):
-    name=vs.find('a', class_='s1Q9rs').text
-    price=vs.find('div', class_='_30jeq3').text
-    
-    data2.append({"name":name,
-        "price":price})
-print(data2)
-print(len(data2))
-pd.DataFrame(data2).to_csv("Flipkart_price.csv")
-
-
 st.sidebar.header("Compare Product between Amazon and Flipkart")
 st.sidebar.image("https://media.giphy.com/media/SpopD7IQN2gK3qN4jS/giphy.gif", width=300)
 
 
 df = pd.read_csv('Flipkart_price.csv')
 st.write(df.head(50))
-#df['price'].apply(type).value_counts()
-#st.write(df.head(50))
-
+da= pd.read_csv('Amazon_price.csv')
 
 option=st.sidebar.selectbox("Choose any option",['costliest','Cheapest','Average'])
 
