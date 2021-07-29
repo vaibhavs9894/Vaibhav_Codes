fae83227abbb6584d4fbb4197faa53b7d08886b5
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-07-29 21:14:40 +0530

    Mini_Project

--- My_Project--Price_Comparision_of_Products/Final/main.py
+++ My_Project--Price_Comparision_of_Products/Final/main.py
@@ -2,48 +2,49 @@ import pandas as pd
 from bs4 import BeautifulSoup
 import requests
 import streamlit as st
+import webbrowser
+from PIL import Image
+import plotly.express as px
 
 st.set_page_config(page_title='Price Comparision of Product')
 
 st.sidebar.header("Compare Product between Amazon and Flipkart")
 st.sidebar.image("https://media.giphy.com/media/SpopD7IQN2gK3qN4jS/giphy.gif", width=300)
 
-
+st.info('Price Scrap from Flipkart are:-')
+img = Image.open("flipkart-trans.png")
+st.image(img, width=200)
 df = pd.read_csv('Flipkart_price.csv')
 st.write(df.head(50))
+
+
+st.info('Price Scrap from Amazon are:-')
+img = Image.open("amazon-trans.png")
+st.image(img, width=200)
 da= pd.read_csv('Amazon_price.csv')
 st.write(da.head(50))
-option=st.sidebar.selectbox("Choose any option",['costliest','Cheapest','Average'])
+
+option=st.sidebar.selectbox("Choose any option",['costliest','Cheapest'])
 
 if option == 'costliest':
     st.header("Costliest Product Price is")
-    #all=[]
-    #all=df['price']
-    #st.write(all)
-    #a=max(all)
-    #st.write(a)
-    p=df['price'].max()
+    st.write('Costliest price on Amazon is:-')
+    p = da['price'].max()
     st.success(p)
-    
-    
+    st.write('Costliest price on Flipkart is:-')
+    q = df['price'].max()
+    st.success(q)
 
 if option == 'Cheapest':
     st.header('Cheapest Product Price is')
-    q=df['price'].min()
-    st.success(q)
-
-
-if option == 'Average':
-    st.header('Average Product Price is')
-    l=len(all)
-    x=0
-    for i in all:
-        x=x+1
-        if i==l:
-            st.write(i)
+    st.header("Costliest Product Price is")
+    st.write('Cheapest price on Amazon is:-')
+    r=da['price'].min()
+    st.success(r)
+    st.write('Cheapest price on Flipkart is:-')
+    s=df['price'].min()
+    st.success(s)  
 
 
 if(st.button("Feeling Lucky")):
     st.image("https://media.giphy.com/media/fUYhyT9IjftxrxJXcE/giphy.gif", width=300)
-
-st.beta_container()
