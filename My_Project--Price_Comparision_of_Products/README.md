9078803a63e0e502d8a7777239c413ce5ec78602
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-08-01 18:37:11 +0530

    Mini_Project:-Price Comparision Of Product Completed

---
+++ My_Project--Price_Comparision_of_Products/README.md
@@ -0,0 +1,37 @@
+<p align="center">
+  <img width="400" height="300" src="https://media.giphy.com/media/l46Cy1rHbQ92uuLXa/giphy.gif">
+</p>
+<br />
+
+# Price Comparision Of Product
+### Compares Product price on Top E-commerce Webites (Amazon & Flipkart)
+
+This is particularly useful when you want to monitor the price of the specific item from eCommerce platforms. Here, in this program, we have targeted two major eCommerce website (Flipkart and Amazon) to find the price of a product. On each execution, Both websites are crawled and the price is obtained.
+
+----------------------------
+
+## Main.py
+
+The file contains the code that implements the product comparison feature. The user may query any number of products' **URL** on **Flipkart** they are interested in comparing
+(e.g. mobile phones of different companies). The user is provided with a datasheet containing comparisons of all the specifications of the products they queried for.
+</br>
+<p align="center">
+  <img width="630" height="280" src="images_used/sheet1.png">
+</p>
+<br />
+
+
+-----------------------------
+## price_comparator.py
+
+The file contains the code that implements the price comparison feature. The user must enter the **name of any product** they wish to compare prices for.They are provided with a 
+datasheet that contains the **prices** and the **link** to the webpage of the **top 5** search results relevant with the product name , on **Amazon** and **Flipkart**.
+</br>
+<p align="center">
+  <img width="600" height="280" src="images_used/sheet2.png">
+</p>
+<br />
+
+
+
+
--- My_Project--Price_Comparision_of_Products/main.py
+++ My_Project--Price_Comparision_of_Products/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from selenium import webdriver
-import requests
 import streamlit as st
 import webbrowser
 from webdriver_manager.chrome import ChromeDriverManager
@@ -8,94 +7,177 @@ import time
 from PIL import Image
 import plotly.express as px
 
-st.set_page_config(page_title='Price Comparision of Product')
-driver = webdriver.Chrome(ChromeDriverManager().install())
+#------------------------------------Enter The Product------------------------------------------#
 
-path = st.text_input('Enter The Product')
 
-l=path
-r="https://www.amazon.in/s?k="
-url =r+l
-driver.get(url)
-time.sleep(2)
+product = st.text_input('Enter The Product')
+
+
+with st.spinner("Welcome!!"):
+    st.markdown("""
+        <style>
+            .mainhead{
+                font-family: Courgette ,Book Antiqua ;
+                #letter-spacing:.1px;
+                word-spacing:1px;
+                color :#4acaff; 
+                text-shadow: 1px -1px 1px white, 1px -1px 2px white;
+                font-size:40px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+    st.markdown("""
+        <style>
+            .detail{
+                font-size:18px;
+                letter-spacing:.1px;
+                word-spacing:1px;
+                font-family:Calibri;
+                color:#3574cb;
+                display:inline-block;
+                }
+        </style>
+    """, unsafe_allow_html=True)
     
-main_area = driver.find_element_by_css_selector("div.s-main-slot.s-result-list.s-search-results.sg-row")
+    st.markdown('<h3 class="mainhead">Price Comparision Of Product </h1> <img src="" />',
+                unsafe_allow_html=True)
+    col1, col2 = st.beta_columns([5, 10])
 
-price = main_area.find_elements_by_css_selector('span.a-price-whole')
-data = []
-for a in price:
-    data.append({
+    with col1:
+
+        st.image('https://media.giphy.com/media/5k1Wu87CzkDfrx0Xwj/giphy.gif')
+
+    with col2:
+
+        col = st.beta_container()
+
+        with col:
+            st.markdown("""
+                <style>
+                    .content{
+                    margin-top: 0%;
+                    letter-spacing:.1px;
+                    word-spacing:1px;
+                    color:Turquoise;
+                    margin-left:5%;}
+                </style>
+            """, unsafe_allow_html=True)
+
+            st.markdown('<p class="content">Hello <br> Welcome To My Project:- Price Comparision Of Product. In this Web scraping with Python, the script searches for a specified product via URL and find out the price at that moment. This is particularly useful when you want to monitor the price of the specific item from eCommerce platforms. Here, in this program, we have targeted two major eCommerce website (Flipkart and Amazon) to find the price of a product. On each execution, Both websites are crawled and the price is obtained.The price of the same product from all the sources are displayed on the console window, so that buyer can see the prices and make the decision to buy from the platform which offers the lowest price! </p>', unsafe_allow_html=True)
+            st.markdown(
+                '<marquee class="content" style = "float:right;"> Made By Vaibhav Shrimali</marquee>', unsafe_allow_html=True)
+
+st.markdown("")
+st.markdown("___")
+
+
+#-------------------------------------------Scraping of price From Amazon and Flipkart--------------------------------------#
+
+
+if product:
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    l=product
+    r="https://www.amazon.in/s?k="
+    url =r+l
+    driver.get(url)
+    time.sleep(2)
+    
+    main_area = driver.find_element_by_css_selector("div.s-main-slot.s-result-list.s-search-results.sg-row")
+
+    price = main_area.find_elements_by_css_selector('span.a-price-whole')
+    data1 = []
+    for a in price:
+        data1.append({
         
-    'price':a.text})
+        'price':a.text})
 
-    pd.DataFrame(data).to_csv("Amazon_price.csv")
-    da= pd.read_csv("Amazon_price.csv")
-
-    da['price'] = da['price'].replace({'₹': '', ',': '', '\(': '-', '\)': ''}, regex=True).astype(float)
-
-    pd.DataFrame(da).to_csv("Amazon_price.csv"  )
-
-#-------------------------------------------Scraping--------------------------------------#
-
-r="https://www.flipkart.com/search?q="
-url =r+l
-driver.get(url)
-time.sleep(2)
-main_area = driver.find_element_by_css_selector("div._36fx1h._6t1WkM._3HqJxg")
-print(main_area)
-price = main_area.find_elements_by_css_selector('div._30jeq3')
-data = []   
-for a in price:
-    data.append({
+        pd.DataFrame(data1).to_csv("Amazon_price.csv")
+        da= pd.read_csv("Amazon_price.csv")
         
-    'price':a.text})
+        
+        #-------------------------Cleaning Price value-------------------------------------#
 
-pd.DataFrame(data).to_csv("flipkart_price.csv")
-df= pd.read_csv("Flipkart_price.csv")
 
-df['price'] = df['price'].replace({'₹': '', ',': '', '\(': '-', '\)': ''}, regex=True).astype(float)
-pd.DataFrame(df).to_csv("Flipkart_price.csv")
+        da['price'] = da['price'].replace({'₹': '', ',': '', '\(': '-', '\)': ''}, regex=True).astype(float)
 
-#-------------------------------------MainPage fro Streamlit--------------------------------------------#
+        pd.DataFrame(da).to_csv("Amazon_price.csv"  )
 
 
 
-st.sidebar.header("Compare Product between Amazon and Flipkart")
-st.sidebar.image("https://media.giphy.com/media/SpopD7IQN2gK3qN4jS/giphy.gif", width=300)
+    r="https://www.flipkart.com/search?q="
+    url =r+l
+    
+    driver.get(url)
+    time.sleep(2)
+    main_area = driver.find_element_by_css_selector("div._36fx1h._6t1WkM._3HqJxg")
+    print(main_area)
+    price = main_area.find_elements_by_css_selector('div._30jeq3')
+    data2 = []   
+    for a in price:
+        data2.append({
+        
+        'price':a.text})
 
-st.info('Price Scrap from Flipkart are:-')
-img = Image.open("flipkart-trans.png")
-st.image(img, width=200)
-df = pd.read_csv('Flipkart_price.csv')
-st.write(df.head(50))
-
-st.info('Price Scrap from Amazon are:-')
-img = Image.open("amazon-trans.png")
-st.image(img, width=200)
-da= pd.read_csv('Amazon_price.csv')
-st.write(da.head(50))
-
-option=st.sidebar.selectbox("Choose any option",['costliest','Cheapest'])
-
-if option == 'costliest':
-    st.header("Costliest Product Price is")
-    st.write('Costliest price on Amazon is:-')
-    p = da['price'].max()
-    st.success(p)
-    st.write('Costliest price on Flipkart is:-')
-    q = df['price'].max()
-    st.success(q)
-
-if option == 'Cheapest':
-    st.header('Cheapest Product Price is')
-    st.header("Costliest Product Price is")
-    st.write('Cheapest price on Amazon is:-')
-    r=da['price'].min()
-    st.success(r)
-    st.write('Cheapest price on Flipkart is:-')
-    s=df['price'].min()
-    st.success(s)  
+    pd.DataFrame(data2).to_csv("flipkart_price.csv")
+    df= pd.read_csv("Flipkart_price.csv")
 
 
-if(st.button("Feeling Lucky")):
-    st.image("https://media.giphy.com/media/fUYhyT9IjftxrxJXcE/giphy.gif", width=300)
+    #-------------------------Cleaning Price value-------------------------------------#
+
+
+    df['price'] = df['price'].replace({'₹': '', ',': '', '\(': '-', '\)': ''}, regex=True).astype(float)
+    pd.DataFrame(df).to_csv("Flipkart_price.csv")
+    
+
+
+
+#-------------------------------------MainPage for Streamlit--------------------------------------------#
+
+    
+    st.info('Price Scrap from Flipkart are:-')
+    img = Image.open("flipkart-trans.png")
+    st.image(img, width=200)
+    df = pd.read_csv('Flipkart_price.csv')
+    st.write(df.head(50))
+
+    st.info('Price Scrap from Amazon are:-')
+    img = Image.open("amazon-trans.png")
+    st.image(img, width=200)
+    da= pd.read_csv('Amazon_price.csv')
+    st.write(da.head(50))
+    
+
+    st.sidebar.header("Price Comparision of Product between Amazon and Flipkart")
+    st.sidebar.image("https://media.giphy.com/media/SpopD7IQN2gK3qN4jS/giphy.gif", width=300)
+
+    option=st.sidebar.selectbox("Choose any option",['Cheapest','costliest'])
+
+    da= pd.read_csv('Amazon_price.csv')
+    df= pd.read_csv("Flipkart_price.csv")
+    if option == 'Cheapest':
+        st.header('Cheapest Product Price is')
+        st.success("Cheapest Price is on Amazon is")
+        r=da['price'].min()
+        st.error(r)
+        st.success('Cheapest price on Flipkart is:-')
+        s=df['price'].min()
+        st.error(s)  
+
+    if option == 'costliest':
+        st.header("Costliest Product Price is")
+        st.success('Costliest price on Amazon is:-')
+        p = da['price'].max()
+        st.error(p)
+        st.success('Costliest price on Flipkart is:-')
+        q = df['price'].max()
+        st.error(q)
+
+
+#---------------------------------------Footer-----------------------------------------#
+
+
+    st.markdown(
+                '<marquee class="content" style = "float:right;"> Visit My gitHub:- https://github.com/vaibhavs9894</marquee>', unsafe_allow_html=True)
+
+
