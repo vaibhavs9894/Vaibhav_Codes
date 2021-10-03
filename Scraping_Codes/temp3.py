1bb581d6bcf033555e72c979d0f8545ad34a0811
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-10-03 10:52:09 +0530

    new beautiflsoup

--- Scraping_Codes/temp3.py
+++ Scraping_Codes/temp3.py
@@ -6,25 +6,28 @@ url=requests.get("https://www.boxofficemojo.com/chart/top_lifetime_gross/?area=X
 html=url.content
 soup=BeautifulSoup(html,"html.parser")
 main=soup.find('div',class_='a-section imdb-scroll-table-inner')
-title=soup.find('a',class_ ='a-link-normal')
-Boxoffice=soup.find('td',class_ ='a-text-right mojo-field-type-money')
+title=main.find_all('a')
+Boxoffice=main.find_all('td',class_ ='a-text-right mojo-field-type-money')
 data=[]
 #for vs in main.find_all('tr', class_='cb-srs-stats-tr'):
   #  name=vs.text
 i=1
 j=2
 a=0
-for v in main.find_all('a'):
-    a=a+1
-    if a==i:
-        i=i+2
-        title=v.text
-    if a==j:
-        j=j+2
-        year=v.text
-        data.append({
-        "Title":title,
-        "year":year})
+for v,p in zip(title,Boxoffice):
+  a=a+1
+  if a==i:
+      i=i+2
+      title=v.text
+  if a==j:
+    j=j+2
+    year=v.text
+    
+    collection=p.text
+    data.append({
+    "Title":title,
+    "year":year,
+    "Box_Office_Collection":collection})
 print(data)
 pd.DataFrame(data).to_csv("Box_Office_Collections.csv")
     
