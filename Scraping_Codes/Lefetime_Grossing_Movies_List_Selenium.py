64a4bbda9ed5cbc5516625957ed20dd70fe9b1d1
Vaibhav Shrimali <vaibhavshrimali9894@gmail.com>
2021-10-04 09:54:51 +0530

    Selenium Scraping

--- Scraping_Codes/Lefetime_Grossing_Movies_List_Selenium.py
+++ Scraping_Codes/Lefetime_Grossing_Movies_List_Selenium.py
@@ -11,16 +11,19 @@ driver.get(url)
 time.sleep(2)
 
 main_area = driver.find_element_by_css_selector("table.a-bordered.a-horizontal-stripes.a-size-base.a-span12.mojo-body-table.mojo-table-annotated.scrolling-data-table")
-movie_titles = main_area.find_elements_by_css_selector("table.a-bordered.a-horizontal-stripes.a-td.a-text-left.mojo-field-type-title")
-movie_Collection = main_area.find_elements_by_css_selector('table.a-bordered.a-horizontal-stripes.a-size-td.a-text-right.mojo-field-type-money')
-print(movie_ti)
+movie_titles = main_area.find_elements_by_css_selector("td.a-text-left.mojo-field-type-title")
+movie_Collection = main_area.find_elements_by_css_selector('td.a-text-right.mojo-field-type-money')
+#print(main_area.text)
+
 data = []
-for title,Collect in zip(movie_titles,movie_Collection):
+for t,c in zip(movie_titles,movie_Collection):
+    
     data.append({
-        "Title":title,
-        "Collection":Collect
+        "Title":t.text,
+        "Lifetime_Grossing":c.text
+        
             })
 
 driver.close()
 print(data)
-pd.DataFrame(data).to_csv("Box_Office_Collection.csv")
+pd.DataFrame(data).to_csv("Top Lifetime Grosses_Movies.csv")
